35037cb71d7d819d65114815033c347d9985c2c2
hyderarain <harain@chicagobooth.edu>
2021-07-10 18:12:04 +0000

    rock setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,24 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
+  def play_rock
+    # write code when user plays rock
+    
+    
+    @comp_move = ["rock", "paper", "scissors"].sample
+    
+    if @comp_move == "rock" 
+      @outcome = "tied"
+    elsif @comp_move == "paper" 
+        @outcome = "lost"
+    elsif  @comp_move == "scissors" 
+        @outcome = "won"
+    end 
+
+    render({:template => "game_templates/user_rock.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
+    self.get("/", { :controller => "application", :action => "homepage"})
+
+    self.get("/rock", { :controller => "application", :action => "play_rock"})
+
+    self.get("/paper", { :controller => "application", :action => "play_rock"})
+
+    self.get("/scissors", { :controller => "application", :action => "play_scissors"})
 
 end
