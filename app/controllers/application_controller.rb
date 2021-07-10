7c4a131ee333f672abe3ebf5b12afcc9436e5983
hyderarain <harain@chicagobooth.edu>
2021-07-10 20:13:37 +0000

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -23,5 +23,39 @@ class ApplicationController < ActionController::Base
     end 
 
     render({:template => "game_templates/user_rock.html.erb"})
+  end
+
+  def play_paper
+    # write code when user plays paper
+    
+    
+    @comp_move = ["rock", "paper", "scissors"].sample
+    
+    if @comp_move == "rock" 
+      @outcome = "won"
+    elsif @comp_move == "paper" 
+        @outcome = "tied"
+    elsif  @comp_move == "scissors" 
+        @outcome = "lost"
+    end 
+
+    render({:template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+    # write code when user plays scissors
+    
+    
+    @comp_move = ["rock", "paper", "scissors"].sample
+    
+    if @comp_move == "rock" 
+      @outcome = "lost"
+    elsif @comp_move == "paper" 
+        @outcome = "won"
+    elsif  @comp_move == "scissors" 
+        @outcome = "tied"
+    end 
+
+    render({:template => "game_templates/user_scissors.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
     self.get("/rock", { :controller => "application", :action => "play_rock"})
 
-    self.get("/paper", { :controller => "application", :action => "play_rock"})
+    self.get("/paper", { :controller => "application", :action => "play_paper"})
 
     self.get("/scissors", { :controller => "application", :action => "play_scissors"})
 
